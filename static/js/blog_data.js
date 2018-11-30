742c186993841a47497f9d434315fccfa7d1aa01
lby1024 <1028524431@qq.com>
2018-11-30 23:10:50 +0800

    1

---
+++ static/js/blog_data.js
@@ -0,0 +1,52 @@
+export default {
+    demos: [
+        {
+            title: '仿饿了么',
+            tags: ['React', 'Redux', 'react-router', 'axios', 'better-scroll'],
+            pic: require('../../src/assets/images/4.png'),
+            link: 'https://github.com/lby1024/ele'
+        },  // '仿饿了么'
+        {
+            title: '后台管理系统',
+            tags: ['React', 'react-router', 'axios', 'antd-design', 'echarts','百度地图', '响应式'],
+            pic: require('../../src/assets/images/6.png'),
+            link: 'https://github.com/lby1024/off'
+        },  // 后台管理系统
+        {
+            title: '音乐播放器',
+            tags: ['vue', 'vuex', 'vue-router', 'better-scroll', 'vue-awesome-swiper', 'axios', 'jsonp'],
+            pic: require('../../src/assets/images/3.png'),
+            link: 'https://github.com/lby1024/music'
+        },  // 音乐播放器
+        {
+            title: '仿简书',
+            tags: ['React', 'Redux', 'react-router', 'axios', '响应式'],
+            pic: require('../../src/assets/images/7.png'),
+            link: 'https://github.com/lby1024/jian_shu'
+        },  // 简书
+        {
+            title: '仿去哪儿网',
+            tags: ['vue', 'vuex', 'vue-router', 'vue-awesome-swiper', 'axios', 'better-scroll'],
+            pic: require('../../src/assets/images/5.png'),
+            link: 'https://github.com/lby1024/travle'
+        },  // 去哪儿网
+        {
+            title: '书单',
+            tags: ['小程序', 'es6'],
+            pic: require('../../src/assets/images/8.png'),
+            link: 'https://github.com/lby1024/books'
+        },  // 书单
+        {
+            title: '计算器',
+            tags: ['小程序', 'es6'],
+            pic: require('../../src/assets/images/1.png'),
+            link: 'https://github.com/lby1024/wechart_calculator'
+        },  // 计算器
+        {
+            title: 'flappy bird',
+            tags: ['canvas', 'es6'],
+            pic: require('../../src/assets/images/2.png'),
+            link: 'https://github.com/lby1024/flappy_bird_for_es6_study'
+        },  // flappy bird
+    ]
+}
